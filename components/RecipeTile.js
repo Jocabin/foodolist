841a758c4ad542aa59d2eaf886bcf8e71e0ca1a4
Jocabin <kobyfar@hotmail.com>
2022-09-08 23:19:06 +0200

    add component RecipeTile

---
+++ components/RecipeTile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {View, Text, StyleSheet} from "react-native";
+
+function RecipeTile() {
+    return (
+
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default RecipeTile
